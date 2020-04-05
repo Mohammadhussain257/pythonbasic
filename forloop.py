ab6c8fc79ad9936bc3a4a797edcea9db9ef8ca7a
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-04-05 21:32:26 +0545

    console for loop

---
+++ forloop.py
@@ -0,0 +1,21 @@
+for letter in 'Hello world':
+    print(letter)
+
+print('*'*10)
+
+for index in range(5):
+    print(index)
+
+print('*'*10)
+
+friends = ['Sanam', 'Ayush', 'Elvin']
+for name in friends:
+    print(name)
+
+print('*'*10)
+
+for index in range(len(friends)):
+    print(friends[index])
+
+print('*'*10)
+
