cfcf8ff8c0c51c157c6aecab5e2dd15375b15930
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-04-05 21:38:58 +0545

    console statement

---
+++ statement.py
@@ -0,0 +1,17 @@
+isMale = True
+isTall = False
+
+if isMale:
+    print('Your are Male')
+else:
+    print('You are not Male')
+
+if isMale or isTall:
+    print('Your are Male or Tall')
+else:
+    print('Your are not male or tall')
+
+if isMale and isTall:
+    print('You are Male and Tall')
+else:
+    print('You are not Male and Tall')
