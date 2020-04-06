cdebb0be1b23d6e8be28eaa01df18e86f482332d
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-04-06 11:42:27 +0545

    list

---
+++ list.py
@@ -0,0 +1,18 @@
+numbers = [9, 8, 7, 6, 5, 1, 6, 5, 8, 4]
+print('*'*20)
+print(numbers)
+print('*'*20)
+friends = ['Elvin', 'Sanam', 'Ayush', 'Amulya']
+friends.sort()
+print(friends)
+print('*'*20)
+friends.extend(numbers)
+print(friends)
+print('*'*20)
+friends.append('Oscar')
+print(friends)
+friends.insert(4, 'Ola')
+print('*'*20)
+print(friends)
+print('*'*20)
+
