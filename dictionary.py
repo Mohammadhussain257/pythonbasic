42d1ed0341d276b9f6f228b56932235de9fb5a09
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-04-06 11:51:32 +0545

    dictonary and tuple

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+dictionaries = {
+    "Sun": "Sunday",
+    "Mon": "Monday",
+    "Tue": "Tuesday",
+    "Wed": "Wednesday",
+    "Thu": "Thursday",
+    "Fri": "Friday",
+    "Sat": "Saturday",
+}
+print(dictionaries['Tue'])
+print(dictionaries['Fri'])
+print(dictionaries)
---
+++ tuple.py
@@ -0,0 +1,10 @@
+# tuple is immutable
+coordinates = (1, 2, 3, 4, 5, 6)
+# coordinates[1]=7 cannot reassign
+print(coordinates[2])
+print(coordinates)
+
+coordinates1 = [(4, 5), (5, 6)]
+print(coordinates1[1])
+print(coordinates1[0:2])
+
