55a19e316b8aa6a707e00e3c0b200b4156d271bf
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-04-06 11:11:22 +0545

    data and veriable

---
+++ dataandveriable.py
@@ -0,0 +1,17 @@
+name = "Jonny"
+age = 35
+is_Male = True
+friends = ['Elvin', 'Sanam', 'Ayush']
+
+print('There once was a man named {} he was {} year old'.format(name, age))
+print('He really liked the name' + name + ',')
+print("but didn't like being " + str(age) + ".")
+
+print(name.upper())
+print(name[3])
+print(name.index('y'))
+print(name.replace('y', 'i'))
+print(friends)
+print(friends[1])
+print(friends[-2])
+print(friends[1:2])
