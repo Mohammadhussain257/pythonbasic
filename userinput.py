2fe1cb66ae7a33bab7aa44d396f11b82642a6376
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-04-06 11:18:03 +0545

    user input

---
+++ userinput.py
@@ -0,0 +1,5 @@
+name = input('Enter your name :')
+age = int(input('Enter your age : '))
+address = input('Enter your address : ')
+print('Hello {} you are {} year old and your address is {}'.format(name, age, address))
+
