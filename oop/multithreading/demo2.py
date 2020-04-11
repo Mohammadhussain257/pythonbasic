ba18f901f545185295ebe2e3761c8d2a039e981b
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-04-11 09:42:57 +0545

    thread demo

---
+++ oop/multithreading/demo2.py
@@ -0,0 +1,14 @@
+from threading import *
+
+
+class MyThread(Thread):
+    def run(self) -> None:
+        for i in range(10):
+            print('Child Thread')
+
+
+t = MyThread()
+t.start()
+
+for i in range(10):
+    print('Main Thread')
