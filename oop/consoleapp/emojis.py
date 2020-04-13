ae9caf7114f52f37b4771806270769ef5799f5d4
mohammadhussain257 <mohammadhusen257@gmail.com>
2020-04-13 12:56:15 +0545

    emojis demo

---
+++ oop/consoleapp/emojis.py
@@ -0,0 +1,10 @@
+message = input(">")
+words = message.split(' ')
+output = ""
+emojis = {
+    ":)": "😊",
+    ":(":"😒"
+}
+for word in words:
+    output +=emojis.get(word, word)+ " "
+print(output)
